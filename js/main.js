80941c90dabf3ed1ad561aac7c9ee02fc0402d75
tigran <tigran.grigoryan.mobile@gamil.com>
2019-09-18 11:22:26 +0400

    Adding game.html file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,81 @@
-$(".right-corder-container-button").hover(function () {
-    $(".long-text").addClass("show-long-text");
-}, function () {
-    $(".long-text").removeClass("show-long-text");
+// FULL SCREEN BTN
+
+function toggleFullscreen(elem) {
+  elem = elem || document.documentElement;
+  if (!document.fullscreenElement && !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
+document.getElementById('btnFullscreen').addEventListener('click', function () {
+  toggleFullscreen();
+});
+
+$(document).ready(function () {
+  $('#btnFullscreen').on("click", function () {
+    if ($(this).hasClass('fas fa-expand-arrows-alt')) {
+      $(this).removeClass('fas fa-expand-arrows-alt');
+      $(this).addClass('fas fa-compress-arrows-alt');
+    } else {
+      $(this).removeClass('fas fa-compress-arrows-alt');
+      $(this).addClass('fas fa-expand-arrows-alt');
+    }
+  });
+});
+
+
+// AUDIO BUTTAN
+
+let track = document.querySelector("#track");
+document.getElementById("musicBtn").onclick = function() {
+  if (!track.paused && !track.ended) {
+    track.pause();
+  } else {
+    track.play();
+  }
+};
+
+$(document).ready(function () {
+  $('#musicBtn').on("click", function () {
+    if ($(this).hasClass('fas fa-music')) {
+      $(this).removeClass('fas fa-music');
+      $(this).addClass('fa fa-pause');
+    } else {
+      $(this).removeClass('fa fa-pause');
+      $(this).addClass('fas fa-music');
+    }
+  });
+});
+
+// SOUND BUTTON
+
+$(document).ready(function () {
+  $('#soundBtn').on("click", function () {
+    if ($(this).hasClass('fas fa-volume-up')) {
+      $(this).removeClass('fas fa-volume-up');
+      $(this).addClass('fas fa-volume-mute');
+    } else {
+      $(this).removeClass('fas fa-volume-mute');
+      $(this).addClass('fas fa-volume-up');
+    }
+  });
 });
