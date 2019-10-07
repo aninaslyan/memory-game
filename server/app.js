6872f6a174555502e1934e2005077f844731765d
aninaslyan <ani.naslyan@gmail.com>
2019-10-07 19:13:36 +0400

    rewriting server to use Koa instead of a usual http request

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
-import http from 'http';
+import Koa from 'koa';
+import Router from 'koa-router';
+import logger from 'koa-logger';
 
-const port = 3000;
-const hostname = 'localhost';
+const app = new Koa();
+const router = new Router();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World!';
 });
 
-server.listen(port, hostname,() => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
